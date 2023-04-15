39ab677ab780fccf4cd7db3c27302962e4569209
ElinaAndreeva <lolaclouds@yandex.ru>
2023-04-15 12:15:49 +0400

    Implemented switches

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        boolean isLeapYearBool = year%4 == 0 && (year % 100 != 0 || year % 400 == 0);
+        int leap;
+        if (isLeapYearBool) {
+            leap = 1;
+        } else {
+            leap = 0;
+        }
+        switch (leap) {
+            case 1:
+                System.out.println("leap");
+            case 0:
+                System.out.println("not leap");
+        }
     }
 }
